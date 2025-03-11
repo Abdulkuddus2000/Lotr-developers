bbded5a83f3fbbf5cff954443d539cff6e4938e6
Abdul Kosimov <abdul.kosimov@student.ap.be>
2025-03-11 06:00:39 +0100

    secition  aangemaakt voor character en voor quotes

--- js/api.js
+++ js/api.js
@@ -1,11 +1,23 @@
 import characters from "./characters.json" with { type: "json" };
 import quotes from "./quotes.json" with { type: "json" };
 
-const ul = document.querySelector('.charactersList');
-characters.docs.forEach(character => {
-  const li = document.createElement('li');
-  li.textContent = character.name;
-  li.style.color = "white";
-  ul.appendChild(li);
-});
+// const ul = document.querySelector('.charactersList');
+// characters.docs.forEach(character => {
+//   const li = document.createElement('li');
+//   li.textContent = character.name;
+//   li.style.color = "white";
+//   ul.appendChild(li);
+// });
+
+//  console.table(characters.docs);
+//  console.table(quotes.docs);
+
+
+for (const character of characters) {
+  for (const quote of quotes) {
+    if (character._id === quote._id) {
+      console.log(character.docs.name, ": ", quote.docs.dialog)
+    }
+  }
+}
 
