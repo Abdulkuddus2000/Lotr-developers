4e1ed8566d801f1b2c2fa759a36caa0fc73f1f15
Abdul Kosimov <abdul.kosimov@student.ap.be>
2025-03-11 06:59:20 +0100

    Nog meer wijzigingen aan favorites.html en js/api.js

--- js/api.js
+++ js/api.js
@@ -1,20 +1,25 @@
 import characters from "./characters.json" with { type: "json" };
 import quotes from "./quotes.json" with { type: "json" };
 
-// const ul = document.querySelector('.charactersList');
-// characters.docs.forEach(character => {
-//   const li = document.createElement('li');
-//   li.textContent = character.name;
-//   li.style.color = "white";
-//   ul.appendChild(li);
-// });
-
+let main = document.querySelector('main');
 
 
 // test met alle karakters en hun quotes
 for (let i = 0; i < characters.docs.length; i++){
   let numberOfQuotes = 0; // teller aanmaken + resetten per nieuw karakter
   let characterName = characters.docs[i].name;
+
+  // section aanmaken per karakter
+  let section = document.createElement('section');
+  section.style.backgroundColor = "grey";
+
+  // h3 aanmaken voor naam van karakter
+  let titleForName = document.createElement('h3');
+  titleForName.textContent = characterName;
+  
+  // h3 toevoegen aan sectie
+  section.appendChild(titleForName);
+
 
   // aantal quotes tellen voor elk karakter
   for (let j = 0; j < quotes.docs.length; j++){
@@ -23,9 +28,15 @@ for (let i = 0; i < characters.docs.length; i++){
       console.log(`${characters.docs[i].name} zei: "${quotes.docs[j].dialog}"`)
     }
   }
+
   // toon het aantal quotes
   if (numberOfQuotes > 0) {
-    console.log(`Aantal quotes van ${characterName}: ${numberOfQuotes}`);
+    // p aanmaken
+    let textForNumberOfQuotes = document.createElement('p');
+    textForNumberOfQuotes.textContent = `Aantal quotes voor ${characterName}: ${numberOfQuotes}`;
+    section.appendChild(textForNumberOfQuotes);
   }
+
+  main.appendChild(section);
 }
 
