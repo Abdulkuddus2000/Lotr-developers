13814dcd378ce91a583133694057a0f74d29f348
Abdul Kosimov <abdul.kosimov@student.ap.be>
2025-03-16 18:19:04 +0100

    laatste commit

--- js/characterDetail.js
+++ js/characterDetail.js
@@ -1,13 +1,12 @@
-import characters from "./characters.json" with { type: "json" };
-import quotes from "./quotes.json" with { type: "json" };
+import charactersData from "./characters.json" with { type: "json" };
+import quotesData from "./quotes.json" with { type: "json" };
 
-
-
-const character = characters.docs.find(char => char.name === "Gandalf");
-const gandalfQuotes = quotes.docs.filter(quote => quote.character === character._id);
+const character = charactersData.docs.find(char => char.name === "Gandalf");
+const gandalfQuotes = quotesData.docs.filter(quote => quote.character === character._id);
 
 // info over het karakter
 const info = document.querySelector(".quotes");
+
 
 // quotes van het karakter
 
