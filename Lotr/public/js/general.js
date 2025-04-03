b51a7ae771d91899ee0606dab3be45a3c5b90982
Abdul <42711396+Abdulkuddus2000@users.noreply.github.com>
2025-04-03 15:52:12 +0000

    dev container aangemaakt + alles omgevormd naar een ejs

---
+++ Lotr/public/js/general.js
@@ -0,0 +1,9 @@
+const customAlert = document.querySelector(".customAlert");
+
+function ShowAlert() {
+    customAlert.style.display = 'flex';
+}
+
+function HideAlert() {   
+    customAlert.style.display = 'none';
+}
