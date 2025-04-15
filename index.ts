f94ba21668b36fe37a76edcaeafaff4a64cef6bb
Abdul Kosimov <abdul.kosimov@student.ap.be>
2025-04-15 21:57:25 +0200

    routers toegevoegd voor alle ejs-pagina's

--- index.ts
+++ index.ts
@@ -17,8 +17,8 @@ app.set("port", process.env.PORT ?? 3000);
 
 app.get("/landing", (req, res) => {
     res.render("landing", {
-        title: "Landing",
-        message: "Landing"
+        title: "landing",
+        message: "landing"
     });
 });
 
@@ -28,6 +28,93 @@ app.get("/registration", (req, res) => {
         message: "registration"
     });
 });
+
+app.get("/account", (req, res) => {
+    res.render("account", {
+        title: "account",
+        message: "account"
+    });
+});
+
+app.get("blacklist", (req, res) => {
+    res.render("blacklist", {
+        title: "blacklist",
+        message: "blacklist"
+    });
+});
+
+app.get("favoriteCharacter", (req, res) => {
+    res.render("favoriteCharacter", {
+        title: "favoriteCharacter",
+        message: "favoritCharacter"
+    });
+});
+
+app.get("favorites", (req, res) => {
+    res.render("favorites", {
+        title: "favorites",
+        message: "favorites"
+    });
+});
+
+app.get("index", (req, res) => {
+    res.render("index", {
+        title: "index",
+        message: "index"
+    });
+});
+
+
+app.get("landing", (req, res) => {
+    res.render("landing", {
+        title: "landing",
+        message: "landing"
+    });
+});
+
+app.get("login", (req, res) => {
+    res.render("login", {
+        title: "login",
+        message: "login"
+    });
+});
+
+
+app.get("quiz", (req, res) => {
+    res.render("quiz", {
+        title: "quiz",
+        message: "quiz"
+    });
+});
+
+app.get("registration", (req, res) => {
+    res.render("registration", {
+        title: "registration",
+        message: "registration"
+    });
+});
+
+app.get("resetPassword", (req, res) => {
+    res.render("resetPassword", {
+        title: "resetPassword",
+        message: "resetPassword"
+    });
+});
+
+app.get("sudden_death", (req, res) => {
+    res.render("sudden_death", {
+        title: "sudden_death",
+        message: "sudden_death"
+    });
+});
+
+app.get("ten_rounds", (req, res) => {
+    res.render("ten_rounds", {
+        title: "ten_rounds",
+        message: "ten_rounds"
+    });
+});
+
 
 app.listen(app.get("port"), () => {
     console.log("Server started on http://localhost:" + app.get("port"));
