3b39b5be361c7a4ba3962b6d0b5c690fb2e82591
Nabil Habrani <s123491@ap.be>
2025-04-15 11:47:42 +0200

    update main

---
+++ index.ts
@@ -0,0 +1,27 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config();
+
+const app : Express = express();
+
+app.use(express.static("public"))
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+
+app.set("port", process.env.PORT ?? 3000);
+
+app.get("/landing", (req, res) => {
+    res.render("landing", {
+        title: "Landing",
+        message: "Landing"
+    })
+});
+
+app.listen(app.get("port"), () => {
+    console.log("Server started on http://localhost:" + app.get("port"));
+});
