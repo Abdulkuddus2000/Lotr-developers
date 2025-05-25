bad8af17cba4b397bfe81e88728907757f845b48
Mohammed Yassin Hamse (s146697) <hamse.mohammedyassin@student.ap.be>
2025-05-25 17:36:59 +0000

    landing css

---
+++ public/js/landingPageALert.js
@@ -0,0 +1,10 @@
+const customAlertLanding = document.querySelector(".customAlertLanding");
+const otherGame = document.querySelector(".otherGame");
+
+function ShowAlert() {
+    customAlertLanding.style.display = 'flex';
+}
+
+function HideAlert() {   
+    customAlertLanding.style.display = 'none';
+}
